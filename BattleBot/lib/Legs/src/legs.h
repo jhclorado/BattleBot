81dc8887939d64251b3c2bb7d980a759cdd6c633
jhclorado <jhclorado@my.cspc.edu.ph>
2026-05-01 15:44:56 +0800

    Fixed compilation error

---
+++ BattleBot/lib/Legs/src/legs.h
@@ -0,0 +1,109 @@
+#ifndef LEGS_H
+#define LEGS_H
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define SERVOMIN 170
+#define SERVOMAX 650
+
+// External PWM driver reference (defined in main.cpp)
+extern Adafruit_PWMServoDriver pwm;
+
+// Convert angle (0-180) to pulse width
+int angleToPulse(int angle) {
+  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+class Joint {
+public:
+  int servoIndex;
+  int currentAngle = 0;
+  int targetAngle = 0;
+  unsigned long lastUpdate = 0;
+  int stepDelay = 10;
+
+  Joint(int index) {
+    servoIndex = index;
+  }
+
+  void setTarget(int angle) {
+    targetAngle = angle;
+  }
+
+  void update() {
+    unsigned long now = millis();
+
+    if (now - lastUpdate >= stepDelay) {
+      lastUpdate = now;
+
+      if (currentAngle < targetAngle) {
+        currentAngle++;
+        pwm.setPWM(servoIndex, 0, angleToPulse(currentAngle));
+      } 
+      else if (currentAngle > targetAngle) {
+        currentAngle--;
+        pwm.setPWM(servoIndex, 0, angleToPulse(currentAngle));
+      }
+    }
+  }
+};
+
+
+class Femur : public Joint {
+public:
+  Femur(int pin) : Joint(pin) {}
+};
+
+class Fibula : public Joint {
+public:
+  Fibula(int pin) : Joint(pin) {}
+};
+
+
+class Limb {
+public:
+  Femur femur;
+  Fibula fibula;
+
+  Limb(int femurPin, int fibulaPin)
+    : femur(femurPin), fibula(fibulaPin) {}
+
+  void setTarget(int femurAngle, int fibulaAngle) {
+    femur.setTarget(femurAngle);
+    fibula.setTarget(fibulaAngle);
+  }
+
+  void update() {
+    femur.update();
+    fibula.update();
+  }
+};
+
+class Quadruped {
+public:
+  Limb frontLeft;
+  Limb frontRight;
+  Limb backLeft;
+  Limb backRight;
+
+  Quadruped(
+    int fl_femur, int fl_fibula,
+    int fr_femur, int fr_fibula,
+    int bl_femur, int bl_fibula,
+    int br_femur, int br_fibula
+  )
+    : frontLeft(fl_femur, fl_fibula),
+      frontRight(fr_femur, fr_fibula),
+      backLeft(bl_femur, bl_fibula),
+      backRight(br_femur, br_fibula) {}
+
+  void update() {
+    frontLeft.update();
+    frontRight.update();
+    backLeft.update();
+    backRight.update();
+  }
+};
+
+#endif
--- BattleBot/src/main.cpp
+++ BattleBot/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include "legs.h"
+#include "comm.h"
+#include "espnow.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Initialize PCA9685 PWM controller
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Quadruped with 8 servos (2 per limb)
+// Servo indices: FL_femur=0, FL_fibula=1, FR_femur=2, FR_fibula=3, 
+//                BL_femur=4, BL_fibula=5, BR_femur=6, BR_fibula=7
+Quadruped robot(0, 1, 2, 3, 4, 5, 6, 7);
+Controller controller(&robot);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  // Initialize PCA9685
+  pwm.begin();
+  pwm.setPWMFreq(60);
+  
+  // Initialize ESP-NOW receiver
+  receive_init();
+  
+  Serial.println("BattleBot Ready");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Check for new ESP-NOW messages
+  char msg[250];
+  uint8_t sender[6];
+  if (get_latest_message(msg, sizeof(msg), sender)) {
+    controller.handlePacket((uint8_t*)msg, strlen(msg));
+  }
+  
+  robot.update();
 }
